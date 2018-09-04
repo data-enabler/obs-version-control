0bb06ca6965d223d324de84d3a4e30671e1f383b
Adrian Mullings <adralemechk@gmail.com>
2018-09-04 00:55:10 -0400

    Fix handling of $repo/some/path strings

--- make_paths_relative.ps1
+++ make_paths_relative.ps1
@@ -1,15 +1,13 @@
 $root = Split-Path -Parent $pwd.Path
-$rootParent = Split-Path -Parent $root
 $root = $root -replace "\\", "/"
-$rootParent = $rootParent -replace "\\", "/"
-$search1 = [regex]::escape($root) + "/OBS-Studio/bin/64bit"
-$search2 = [regex]::escape($rootParent)
+$search1 = [regex]::escape($root) + "/OBS-Studio/bin/64bit/root"
+$search2 = [regex]::escape($root)
 Get-ChildItem "..\OBS-Settings\scenes" -Filter *.json |
 Foreach-Object {
     $file = $_.FullName
     Write-Output $file
     (Get-Content $file) |
-    Foreach-Object { $_ -ireplace $search1, "." } |
-    Foreach-Object { $_ -ireplace $search2, "." } |
+    Foreach-Object { $_ -ireplace $search1, "./root" } |
+    Foreach-Object { $_ -ireplace $search2, "./root" } |
     Set-Content $file
 }
