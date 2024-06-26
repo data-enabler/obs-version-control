227b2ca287452f2db67a7e3136f1aca004d9acc8
Adrian Mullings <adralemechk@gmail.com>
2024-06-26 09:09:08 -0400

    Fix config sorting in recent OBS versions
    Fields used to be sorted in the config JSON, but now they're not.
    Luckily jq makes it easy to sort fields.
    
    Also added more descriptive text in script output.

--- make_paths_relative.ps1
+++ make_paths_relative.ps1
@@ -5,7 +5,7 @@ $search2 = [regex]::escape($root)
 Get-ChildItem "..\OBS-Settings\scenes" -Filter *.json |
 Foreach-Object {
     $file = $_.FullName
-    Write-Output $file
+    Write-Output "Making paths relative for $file"
     (Get-Content $file) |
     Foreach-Object { $_ -ireplace $search1, "./root" } |
     Foreach-Object { $_ -ireplace $search2, "./root" } |
--- sort_sources.ps1
+++ sort_sources.ps1
@@ -1,9 +1,8 @@
 Get-ChildItem "..\OBS-Settings\scenes" -Filter *.json |
 Foreach-Object {
     $file = $_.FullName
-    Write-Output $file
+    Write-Output "Sorting fields in $file"
     Get-Content $file |
-    jq --indent 4 '.sources |= sort_by(.name)' |
-    jq --indent 4 '.groups |= sort_by(.name)' |
+    jq --sort-keys --indent 4 '.sources |= sort_by(.name) | .groups |= sort_by(.name)' |
     Set-Content $file
 }
